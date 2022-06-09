f231792a3d77ca92a31529605e780a12b46c68e2
Glory <gloryomosomwan@gmail.com>
2022-06-09 16:24:51 -0600

    Added functionality to create Jimp versions of the reddit avatar - Figured out how to create a Jimp image using the bitmap from a regular PNG file - Cleaned up 'jimp.js' - Removed the 'image-js' and 'canvas' packages

--- jimp.js
+++ jimp.js
@@ -1,29 +1,23 @@
 const Jimp = require('jimp');
 const { title, comment } = require('./dummy');
-require('dotenv').config();
-const fontPath = process.env.FONT_PATH;
+const width = 900;
 
-async function main() {
-    try {
-        const image = await Jimp.read('images/reddit.png');
-
-        // Render title
-        const titlefont = await Jimp.loadFont(fontPath);
-        image.print(titlefont, 65, 40, title, 800);
-
-        // Render upvotes
-        const upvotefont = await Jimp.loadFont(Jimp.FONT_SANS_16_WHITE);
-        image.print(upvotefont, 13, 45, "2.1k", 900);
-
-        // Render author
-        const authorfont = await Jimp.loadFont(Jimp.FONT_SANS_16_WHITE);
-        image.print(authorfont, 263, 14, "badblackguy");
-
-        // Write image to file
-        const hmm = await image.write('images/test.png');
-    } catch (error) {
-        console.log(error);
-    }
+async function createCanvas(height) {
+    const canvas = new Jimp(width, height, "#1A1A1B", (err, image) => {
+        image.write('resources/canvas.png');
+    });
 }
 
-main();
+async function blit() {
+    const canvas = await Jimp.read('resources/canvas.png');
+
+    // Create a Jimp version of the avatar
+    const avatarpng = await Jimp.read('resources/avatar.png');
+    const bitmap = avatarpng.bitmap;
+    const avatar = new Jimp({ data: bitmap["data"], width: bitmap["width"], height: bitmap["height"] }, (err, image) => {
+        avatar.write('resources/jimp-avatar.png');
+    });
+}
+
+createCanvas(200);
+blit();
