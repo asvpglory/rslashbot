7732cc721d84169d22a41e25b2489e4f7e2fa328
Glory <gloryomosomwan@gmail.com>
2022-06-08 23:06:12 -0600

    Changed payload structure The data being returned is now just an object with the metadata and body of text that I need, same with the comments

--- reddit.js
+++ reddit.js
@@ -8,12 +8,19 @@ const r = new snoowrap({
 
 async function fetchData() {
     // The data to be returned (The submission and its comments (without replies))
-    let data = [];
+    let submission = {};
     // Get the first submission on r/AskReddit when sorting by 'top'
     const submissionListing = await r.getSubreddit('askReddit').getTop({
         limit: 1
     });
-    data.push(submissionListing);
+
+    submission['title'] = submissionListing[0].title;
+    submission['subreddit'] = submissionListing[0].subreddit_name_prefixed;
+    submission['score'] = submissionListing[0].score;
+    submission['author'] = submissionListing[0].author.name;
+    submission['commentCount'] = submissionListing[0].num_comments;
+    // created_utc
+
     // Get the actual comments object
     const commentListing = await submissionListing[0].comments;
     // Fetch the comments inside the comments object from reddit
@@ -22,13 +29,18 @@ async function fetchData() {
         // skipReplies: false
     });
     // For every comment, print that comment with 0 reply depth (no replies)
+    submission['comments'] = [];
     for (dirtyComment of comments) {
         const cleanComment = await dirtyComment.expandReplies({
             depth: 0
         });
-        data.push(cleanComment);
+        submission.comments.push({
+            author: cleanComment.author.name,
+            body: cleanComment.body,
+            score: cleanComment.score
+        });
     }
-    return data;
+    return submission;
 }
 
 fetchData()
---
+++ utilities.js
@@ -0,0 +1,3 @@
+// Function that turns epoch time into a "__ hours ago" string
+
+// Function that shortens larger numbers (e.g. 20423 --> 20.4k)
