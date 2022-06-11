f23b263fc2a15c931057f291709c2c7051ca8255
Glory <gloryomosomwan@gmail.com>
2022-06-11 17:35:47 -0600

    Improved 'fabric.js' clarity - Cleaned up 'fabric.js' (see previous commit)

--- fabric.js
+++ fabric.js
@@ -2,21 +2,23 @@ const fs = require('fs');
 const { lightSilver, eerieBlack, oldSilver, jet } = require('./palette');
 fabric = require('fabric').fabric;
 
-module.exports = (comment, author, timeago, commentScore, commentActions, commentIndex) => {
+module.exports = (commentAuthor, commentTimeago, commentText, commentScore, commentActions, commentIndex) => {
     loadFonts();
-    const text = loadText(comment);
+    const text = loadCommentText(commentText);
     const textHeight = text.height;
     const canvas = renderCanvas(textHeight);
-    renderCommentAvatar(canvas, commentIndex);
-    renderText(canvas, text, commentIndex);
-    renderTrail(canvas, textHeight, commentIndex);
-    const authorTextWidth = renderCommentAuthor(canvas, author, commentIndex);
-    renderCommentTimeago(canvas, authorTextWidth, timeago, commentIndex);
 
-    // Bottom row
-    renderUpvote(canvas, textHeight, commentIndex);
+    // Main rendering
+    renderCommentAvatar(canvas, commentIndex);
+    renderCommentText(canvas, text, commentIndex);
+    renderTrail(canvas, textHeight, commentIndex);
+    const commentAuthorTextWidth = renderCommentAuthor(canvas, commentAuthor, commentIndex);
+    renderCommentTimeago(canvas, commentAuthorTextWidth, commentTimeago, commentIndex);
+
+    // Bottom row rendering
+    renderCommentUpvoteIcon(canvas, textHeight, commentIndex);
     const scoreWidth = renderCommentScore(canvas, textHeight, commentScore, commentIndex);
-    renderDownvote(canvas, textHeight, scoreWidth, commentIndex);
+    renderCommentDownvoteIcon(canvas, textHeight, scoreWidth, commentIndex);
     renderCommentActions(canvas, textHeight, scoreWidth, commentActions, commentIndex);
 };
 
@@ -44,8 +46,8 @@ function loadFonts() {
     return null;
 }
 
-function loadText(comment) {
-    const text = new fabric.Textbox(comment, {
+function loadCommentText(comment) {
+    const commentText = new fabric.Textbox(comment, {
         width: 760,
         top: 64,
         left: 67,
@@ -54,7 +56,7 @@ function loadText(comment) {
         fontFamily: 'Noto Sans',
         fontWeight: 'Medium'
     });
-    return text;
+    return commentText;
 }
 
 function renderCanvas(textHeight) {
@@ -81,20 +83,20 @@ function renderCommentAvatar(canvas, commentIndex) {
 }
 
 function renderCommentAuthor(canvas, author, commentIndex) {
-    const text = new fabric.Text(author, {
+    const commentAuthorText = new fabric.Text(author, {
         top: 30,
         left: 67,
         fill: lightSilver,
         fontSize: 16,
-        fontFamily: "IBMPlexSans",
+        fontFamily: "IBM Plex Sans",
         fontWeight: "SemiBold"
     });
-    write(canvas, text, commentIndex);
-    return text.width;
+    write(canvas, commentAuthorText, commentIndex);
+    return commentAuthorText.width;
 }
 
 function renderCommentTimeago(canvas, offset, timeago, commentIndex) {
-    const text = new fabric.Text(timeago, {
+    const commentTimeagoText = new fabric.Text(timeago, {
         top: 30,
         left: offset + 73,
         fill: oldSilver,
@@ -102,18 +104,18 @@ function renderCommentTimeago(canvas, offset, timeago, commentIndex) {
         fontFamily: "Noto Sans",
         fontWeight: "Medium"
     });
-    write(canvas, text, commentIndex);
+    write(canvas, commentTimeagoText, commentIndex);
 }
 
-function renderText(canvas, text, commentIndex) {
-    write(canvas, text, commentIndex);
+function renderCommentText(canvas, commentText, commentIndex) {
+    write(canvas, commentText, commentIndex);
     return null;
 }
 
-function renderTrail(canvas, height, commentIndex) {
+function renderTrail(canvas, textHeight, commentIndex) {
     const trail = new fabric.Rect({
         width: 2,
-        height: height,
+        height: textHeight,
         fill: jet,
         top: 65,
         left: 35
@@ -122,55 +124,55 @@ function renderTrail(canvas, height, commentIndex) {
     return null;
 }
 
-function renderUpvote(canvas, textHeight, commentIndex) {
+function renderCommentUpvoteIcon(canvas, textHeight, commentIndex) {
     const src = 'file://' + __dirname + '/resources/upvote.png';
     fabric.util.loadImage(src, function (img) {
-        const upvote = new fabric.Image(img);
-        upvote.set({
+        const commentUpvoteIcon = new fabric.Image(img);
+        commentUpvoteIcon.set({
             left: 65,
             top: textHeight + 78,
         });
-        upvote.scale(0.025);
-        write(canvas, upvote, commentIndex);
+        commentUpvoteIcon.scale(0.025);
+        write(canvas, commentUpvoteIcon, commentIndex);
     });
 }
 
 function renderCommentScore(canvas, textHeight, commentScore, commentIndex) {
-    const text = new fabric.Text(commentScore, {
+    const commentScoreText = new fabric.Text(commentScore, {
         left: 95,
         top: textHeight + 83,
         fill: lightSilver,
         fontSize: 15,
-        fontFamily: "IBMPlexSans",
+        fontFamily: "IBM Plex Sans",
         fontWeight: "Bold"
     });
-    write(canvas, text, commentIndex);
-    return text.width;
+    write(canvas, commentScoreText, commentIndex);
+    return commentScoreText.width;
 }
 
-function renderDownvote(canvas, textHeight, scoreWidth, commentIndex) {
+function renderCommentDownvoteIcon(canvas, textHeight, scoreWidth, commentIndex) {
     const src = 'file://' + __dirname + '/resources/upvote.png';
     fabric.util.loadImage(src, function (img) {
-        const downvote = new fabric.Image(img);
-        downvote.set({
+        const commentDownvoteIcon = new fabric.Image(img);
+        commentDownvoteIcon.set({
             left: scoreWidth + 102,
             top: textHeight + 78,
         });
-        downvote.scale(0.025);
-        downvote.rotate(180);
-        write(canvas, downvote, commentIndex);
+        commentDownvoteIcon.scale(0.025);
+        commentDownvoteIcon.rotate(180);
+        write(canvas, commentDownvoteIcon, commentIndex);
     });
 }
 
 function renderCommentActions(canvas, textHeight, scoreWidth, commentActions, commentIndex) {
-    const text = new fabric.Text(commentActions, {
+    const commentActionsText = new fabric.Text(commentActions, {
         left: scoreWidth + 140,
         top: textHeight + 83,
         fill: oldSilver,
         fontSize: 15,
-        fontFamily: "IBMPlexSans",
+        fontFamily: "IBM Plex Sans",
         fontWeight: "Bold"
     });
-    write(canvas, text, commentIndex);
+    write(canvas, commentActionsText, commentIndex);
     return null;
 }
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const { postSubreddit, postAuthor, postTimeago, postText, postScore, postCommentAmount, postActions } = require('./dummy');
+const { postSubreddit, postAuthor, postTimeago, postText, postScore, postCommentAmount, postActions, commentAuthor, commentTimeago, commentText, commentScore, commentActions } = require('./dummy');
 const renderPost = require('./post');
 
-renderPost(postSubreddit, postAuthor, postTimeago, postText, postScore, postCommentAmount, postActions);
+// renderPost(postSubreddit, postAuthor, postTimeago, postText, postScore, postCommentAmount, postActions);
 
-// const renderComment = require('./fabric');
-// const commentNo = 3;
+const renderComment = require('./fabric');
+const commentNo = 3;
 // for (let i = 1; i <= commentNo; i++) {
-//     render(comment, author, timeago, commentScore, commentActions, i);
+renderComment(commentAuthor, commentTimeago, commentText, commentScore, commentActions, 0);
 // }
