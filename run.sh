2c3bac161106d433702394238c300095bb244766
Glory <gloryomosomwan@gmail.com>
2022-06-19 16:13:18 -0600

    Organized the log output for the bash scripts

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
+echo " "
+echo "*** Running script ***"
+echo date
+echo " "
 cd ~/src/rslashbot
 /home/glory/.nvm/versions/node/v16.15.1/bin/node ~/src/rslashbot/index.js
+echo "*** Script done ***"
--- update.sh
+++ update.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+echo " "
+echo "*** Updating project directory ***"
+echo date
+echo " "
 export HOME=/home/glory
 eval `ssh-agent`
 ssh-add
 cd ~/src/rslashbot
 git pull
+echo "*** Finished updating ***"
