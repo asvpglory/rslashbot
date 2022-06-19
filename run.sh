32a9ce2c59240c5de9dd3effb9c74c1d0eff6dc1
Glory <gloryomosomwan@gmail.com>
2022-06-19 16:21:22 -0600

    Updated shell scripts

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
-echo " "
 echo "*** Running script ***"
-echo date
+date
 echo " "
 cd ~/src/rslashbot
 /home/glory/.nvm/versions/node/v16.15.1/bin/node ~/src/rslashbot/index.js
+echo " "
 echo "*** Script done ***"
+echo " "
--- update.sh
+++ update.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-echo " "
 echo "*** Updating project directory ***"
-echo date
+date
 echo " "
 export HOME=/home/glory
 eval `ssh-agent`
 ssh-add
 cd ~/src/rslashbot
 git pull
+echo " "
 echo "*** Finished updating ***"
+echo " "
