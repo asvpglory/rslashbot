575c89625f77559839dba59fe6c7fab0c1261f04
Glory <gloryomosomwan@gmail.com>
2022-06-19 15:50:42 -0600

    Committing files the bot uses that haven't been tracked

---
+++ dummy.js
@@ -0,0 +1,21 @@
+// Dummy text file
+
+
+// Post data
+module.exports.postSubreddit = "r/AskReddit";
+module.exports.postAuthor = "u/lorddestroyer";
+module.exports.postTimeago = "∙ 19 hrs. ago";
+module.exports.postText = "What is your favourite story? ";
+module.exports.postScore = "9.8k";
+module.exports.postCommentAmount = "3.9K";
+module.exports.postActions = "@rslashbot v1.0.0";
+
+// Comment data
+module.exports.commentTimeago = "∙ 19 hr. ago";
+module.exports.commentAuthor = "schnit123";
+module.exports.commentText = `It's actually frightening how many people feed their dogs chocolate and sweets. I can't tell you how many times I've had customers tell me that the chocolate biscuits they are buying are for the dog, I try and tell them how dangerous it is but no one ever listens because "it's his/her favourite kind and they have been eating them for years". Family always moan that we are too strict with our dogs diet but he's 14, in perfect health and still acts like a puppy while most dogs I know of his age have serious health issues. Pet ownership is a serious business, they depend on you for literally everything. A child will grow up and be able to take responsibility for their own diet but your pet never will.`;
+module.exports.commentScore = "2.4k";
+module.exports.commentActions = "Reply    Give Award    Share    Report    Save    Follow    @rslashbot v2.3";
+
+
+// module.exports.comment = "When I was a kid, we had a guinness book of records lying around that we liked to browse in while bored. There was this one guy in it who held a record for most bikes eaten. No, you did read that right. He ground up a bike and slowly consumed it over I don't know how long a time.";
