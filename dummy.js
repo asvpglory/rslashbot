d5abada65ccaf8c8696d809706a37f792724f6fa
Glory <gloryomosomwan@gmail.com>
2022-06-19 19:17:40 -0600

    Testing cronjobs

--- dummy.js
+++ dummy.js
@@ -15,7 +15,7 @@ module.exports.commentTimeago = "∙ 19 hr. ago";
 module.exports.commentAuthor = "schnit123";
 module.exports.commentText = `It's actually frightening how many people feed their dogs chocolate and sweets. I can't tell you how many times I've had customers tell me that the chocolate biscuits they are buying are for the dog, I try and tell them how dangerous it is but no one ever listens because "it's his/her favourite kind and they have been eating them for years". Family always moan that we are too strict with our dogs diet but he's 14, in perfect health and still acts like a puppy while most dogs I know of his age have serious health issues. Pet ownership is a serious business, they depend on you for literally everything. A child will grow up and be able to take responsibility for their own diet but your pet never will.`;
 module.exports.commentScore = "2.4k";
-module.exports.commentActions = "Reply    Give Award    Share    Report    Save    Follow    @rslashbot v2.3";
+module.exports.commentActions = "Reply    Give Award    Share    Report    Save    Follow    @rslashbot v1.0.0";
 
 
 // module.exports.comment = "When I was a kid, we had a guinness book of records lying around that we liked to browse in while bored. There was this one guy in it who held a record for most bikes eaten. No, you did read that right. He ground up a bike and slowly consumed it over I don't know how long a time.";
